7ea1e282bc00765f37270f8c869f7be28ff689a7
unixkai1111 <gamo.kai.1111@gmail.com>
2022-10-15 12:50:51 +0900

    stopwatch color changed

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  { name: "買い物", condition: "undone" },
+  { name: "歯医者", condition: "done" },
+]);
+const newTaskName = ref("");
+const newTaskCondition = ref(0);
+</script>
+
+<template>
+  <div>Todolist</div>
+  <div v-for="item in items" :key="item.name">
+    <div class="name">名前:</div>
+  </div>
+</template>
+
+<style></style>
